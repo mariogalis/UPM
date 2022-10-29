07e917e196c2cf66e6a94bcdcb2d7edc70f3c913
mario <mario@marios-MacBook-Air.local>
2022-10-29 12:06:39 +0200

    arreglofallo

--- Piscina Chiquitina/ft_print_comb.c
+++ Piscina Chiquitina/ft_print_comb.c
@@ -1,13 +1,3 @@
-#include <unistd.h>
-
-void	print(int i, int j, int k)
-{
-	write(1, &i, 1);
-	write(1, &j, 1);
-	write(1, &k, 1);
-	if (i != '7' || j != '8' || k != '9')
-		write(1, ", ", 2);
-}
 
 void	ft_print_comb(void)
 {
